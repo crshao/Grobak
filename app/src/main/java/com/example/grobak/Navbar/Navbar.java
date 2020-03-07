3376867acd6efbc6cb3b8375823713ca71fdf6e0
hao <calvindon08@gmail.com>
2020-03-07 13:56:17 +0700

    navbar3

--- app/src/main/java/com/example/grobak/Navbar/Navbar.java
+++ app/src/main/java/com/example/grobak/Navbar/Navbar.java
@@ -1,10 +1,14 @@
 package com.example.grobak.Navbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.grobak.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Navbar extends AppCompatActivity {
 
@@ -13,6 +17,28 @@ public class Navbar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navbar);
 
+        private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+                = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                String tag = "";
+                switch(item.getItemId())
+                {
+                    case R.id.home:
 
+                        break;
+                    case R.id.resep:
+                        break;
+                    case R.id.produk:
+                        break;
+                    case R.id.menu:
+                        break;
+                    default:
+                        break;
+                }
+                return loadFrag(getVisibleFragment(), fragment, tag);
+            }
+        }
     }
 }
