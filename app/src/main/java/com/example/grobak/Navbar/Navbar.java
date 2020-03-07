db45ef2b656b0b7115e59b2b0f14497e2b60e056
hao <calvindon08@gmail.com>
2020-03-07 13:48:48 +0700

    LayoutNavbar2

--- app/src/main/java/com/example/grobak/Navbar/Navbar.java
+++ app/src/main/java/com/example/grobak/Navbar/Navbar.java
@@ -12,5 +12,7 @@ public class Navbar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navbar);
+
+
     }
 }
