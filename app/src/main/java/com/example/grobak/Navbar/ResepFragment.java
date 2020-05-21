5772e4824bcea4221e422676a0cc86d16120601f
Bagas Immanuel <bagasimmanuel00@gmail.com>
2020-05-21 14:21:53 +0700

    Fragment Resep Push Pertama

--- app/src/main/java/com/example/grobak/Navbar/ResepFragment.java
+++ app/src/main/java/com/example/grobak/Navbar/ResepFragment.java
@@ -4,19 +4,27 @@ package com.example.grobak.Navbar;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.grobak.Data.DataResep;
+import com.example.grobak.Data.Resep;
+import com.example.grobak.HomeResepRecyclerViewAdapter;
 import com.example.grobak.R;
+import com.example.grobak.ResepResepRecyclerViewAdapter;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ResepFragment extends Fragment {
 
-
+    private ArrayList<Resep> mResep = new ArrayList<>();
     public ResepFragment() {
         // Required empty public constructor
     }
@@ -26,7 +34,17 @@ public class ResepFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resep, container, false);
+        View view = inflater.inflate(R.layout.fragment_resep, container, false);
+        mResep.addAll(DataResep.getListData());
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.resepResepRecyclerView);
+        recyclerView.setLayoutManager(layoutManager);
+        ResepResepRecyclerViewAdapter adapter = new ResepResepRecyclerViewAdapter(mResep,getActivity());
+
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/example/grobak/ResepResepRecyclerViewAdapter.java
@@ -0,0 +1,69 @@
+package com.example.grobak;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.grobak.Data.Resep;
+
+import java.util.ArrayList;
+
+public class ResepResepRecyclerViewAdapter extends RecyclerView.Adapter<ResepResepRecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<Resep> mResep = new ArrayList<>();
+    private Context mContext;
+
+    public ResepResepRecyclerViewAdapter(ArrayList<Resep> mBarangSatuan, Context mContext) {
+        this.mResep = mBarangSatuan;
+        this.mContext = mContext;
+    }
+
+
+    @NonNull
+    @Override
+    public ResepResepRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.resep_resep_list_item, parent, false);
+        return new ResepResepRecyclerViewAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ResepResepRecyclerViewAdapter.ViewHolder holder, int position) {
+        holder.image_resep.setImageResource(mResep.get(position).getImage());
+        holder.namaResep.setText(mResep.get(position).getNama());
+        holder.deskripsiResep.setText(mResep.get(position).getDetail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mResep.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image_resep;
+        TextView namaResep;
+        TextView deskripsiResep;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image_resep = itemView.findViewById(R.id.image);
+            namaResep = itemView.findViewById(R.id.namaResep);
+            deskripsiResep = itemView.findViewById((R.id.deskripsiResep));
+
+            itemView.setOnClickListener(v -> {
+                int pos = getAdapterPosition();
+                Resep resep = mResep.get(pos);
+                Toast.makeText(mContext,"Detail" + resep.getNama(),Toast.LENGTH_SHORT).show();
+            });
+
+
+        }
+    }
+}
