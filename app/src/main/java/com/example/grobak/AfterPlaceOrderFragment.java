aa7d3e3dea8c5b680ff7e5e185cee8dff172fc8d
hao <calvindon08@gmail.com>
2020-05-24 00:02:29 +0700

    UI dan Alur fragment after place order fixed

--- app/src/main/java/com/example/grobak/AfterPlaceOrderFragment.java
+++ app/src/main/java/com/example/grobak/AfterPlaceOrderFragment.java
@@ -9,14 +9,18 @@
 package com.example.grobak;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.button.MaterialButton;
 
 public class AfterPlaceOrderFragment extends Fragment {
 
@@ -33,6 +37,18 @@ public class AfterPlaceOrderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_after_place_order, container, false);
+        View view = inflater.inflate(R.layout.fragment_after_place_order, container, false);
+        MaterialButton button = view.findViewById(R.id.btn_home);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Home", "Home");
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
